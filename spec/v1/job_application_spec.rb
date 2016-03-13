cf47a94c3d87a4fb88da067b67cd352af342c8b9
utkarsh <utkarshrai003@gmail.com>
2016-03-13 17:04:23 +0530

    tests added and code rectified

--- spec/v1/job_application_spec.rb
+++ spec/v1/job_application_spec.rb
@@ -4,42 +4,44 @@ RSpec.describe Api::V1::JobApplicationsController , :type => :controller do
 
   before(:each) do
     @applicant = FactoryGirl.create(:applicant)
+    @job_application1 = @applicant.job_applications.create(:job_id => 1)
+    @job_application2 = @applicant.job_applications.create(:job_id => 2)
+    @job_application3 = FactoryGirl.create(:applicant).job_applications.create(:job_id => 3)
     @job = FactoryGirl.create(:job)
-    #@applicant = double(:applicant)
-
   end
 
-  describe "get #show" do 
-    
-    context "authorized" do
+  def stub_authorization
+    allow(request.env['warden']).to receive(:authenticate_applicant!).and_return(true)
+    allow(controller).to receive(:current_applicant).and_return(@applicant)
+  end
 
+  describe "get #index" do
+    context "when applicant is authorized"
       before do
-        allow(request.env['warden']).to receive(:authenticate_applicant!).and_return(true)
-        allow(controller).to receive(:current_applicant).and_return(@applicant)
+        stub_authorization
       end
 
-      it "allows authenticated applicant only" do
-        get :show , :id => 1
+      it "allows authorized applicants only" do
+        get :index
         expect(response.status).to eq(200)
       end
-    end
 
-    it "doesn't allows unauthenticated applicant" do
-      get :show , :id => 1
-      expect(response.status).to eq(401)
-    end
-
-	end
+      it "should give only current applicant's job applications" do
+        get :index
+        json = JSON.parse(response.body)
+        expect(json["code"]).to eq(1000)
+        expect(json["body"].all? {|x| x["applicant"]["id"] == @applicant.id}).to eq(true)        
+      end
+  end  
 
   describe "post #create" do
-    context "authorized" do
 
+    context "when applicant is authorized" do
       before do
-        allow(request.env['warden']).to receive(:authenticate_applicant!).and_return(true)
-        allow(controller).to receive(:current_applicant).and_return(@applicant)
+        stub_authorization
       end
 
-      it "should create job application for given job" do
+      it "can apply for a given job" do
         post :create , :id => "#{@job.id}"
         json = JSON.parse(response.body)
         expect(response.status).to eq(200)
@@ -53,14 +55,14 @@ RSpec.describe Api::V1::JobApplicationsController , :type => :controller do
         expect(json["body"]["applicant"]["id"]).to eq(@applicant.id)
       end
 
-      it "cannot be applied for job that does not exists" do
+      it "cannot apply for job that doesn't exists" do
         post :create , :id => nil
         json = JSON.parse(response.body)
         expect(response.status).to eq(200)
         expect(json["code"]).to eq(1200)
       end
 
-      it "cannot be applied twice for the same job" do
+      it "cannot apply twice for the same job" do
         @applicant.job_applications.create(:job_id => @job.id)
         post :create , :id => "#{@job.id}"
         json = JSON.parse(response.body)
@@ -74,14 +76,47 @@ RSpec.describe Api::V1::JobApplicationsController , :type => :controller do
         expect(json["code"]).to eq(1000)
         expect(json["body"]["status"]).to eq("pending")
       end
-
-
     end
 
+    context "when user is unauthorized" do
       it "should not allow unauthenticated applicant" do
-        post :create , :id => 1
+        post :create , :id => "#{@job.id}"
         expect(response.status).to eq(401)
       end
-  end
+    end
+  end  
+
+  describe "get #show" do 
+    
+    context "when applicant is authorized" do
+      before do
+        stub_authorization
+      end
+
+      it "allows authenticated applicant only" do
+        get :show , :id => "#{@job.id}"
+        expect(response.status).to eq(200)
+      end
+
+      it "should give applicant his resource only" do
+        get :show , :id => "#{@job_application1.id}"
+        json = JSON.parse(response.body)
+        expect(json["code"]).to eq(1000)
+      end
+
+      it "should not give applicant someone else resource" do
+        get :show , :id => "#{@job_application3.id}"
+        json = JSON.parse(response.body)
+        expect(response.status).to eq(401)        
+      end
+    end
+
+    context "when applicant is unauthorized" do      
+      it "doesn't allows unauthenticated applicant" do
+        get :show , :id => "#{@job.id}"
+        expect(response.status).to eq(401)
+    end
+    end
+	end
 
 end
